18b115f8f8b7258eab6d6d5e1a294f4a8e513d56
Diego Castellanos <diegocastellanos551@gmail.com>
2023-02-22 17:43:30 -0500

    modificacion de la segunda solucion

--- res2.js
+++ res2.js
@@ -1,6 +1,7 @@
-const form = document.getElementById('contact-form');
+const form = document.getElementById('signin-form');
+const submitBtn = document.querySelector('.btn.btn-style.btn-primary');
 
-form.addEventListener('submit', function(event) {
+submitBtn.addEventListener('click', function(event) {
   const name = document.getElementById('w3lName').value.trim();
   const subject = document.getElementById('w3lSubject').value.trim();
   const email = document.getElementById('w3lSender').value.trim();
@@ -12,3 +13,4 @@ form.addEventListener('submit', function(event) {
     alert('Por favor complete todos los campos antes de enviar el formulario.');
   }
 });
+
